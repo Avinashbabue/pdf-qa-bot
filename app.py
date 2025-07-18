ea5fc94418b130dc94250126b8fe9827b621b562
Avinashbabue <avinashbabue1926@gmail.com>
2025-07-18 13:39:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from src.helper import load_pdf, split_text, download_hugging_face_embeddings
 from langchain.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
-from langchain.llms import CTransformers
 from langchain.chains import RetrievalQA
+from langchain.llms import Ollama  
 from src.prompt import prompt_template
 
 # Load and process documents (do this only once using st.cache_resource)
@@ -21,11 +21,8 @@ vectorstore = build_vectorstore()
 PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 chain_type_kwargs = {"prompt": PROMPT}
 
-llm = CTransformers(
-    model="Model/llama-2-7b-chat.ggmlv3.q4_0.bin",
-    model_type="llama",
-    config={'max_new_tokens': 512, 'temperature': 0.8}
-)
+
+llm = Ollama(model="llama2") 
 
 qa = RetrievalQA.from_chain_type(
     llm=llm,
@@ -35,7 +32,7 @@ qa = RetrievalQA.from_chain_type(
     chain_type_kwargs=chain_type_kwargs
 )
 
-st.title("RAG QA Chatbot")
+st.title("🧠 RAG QA Chatbot")
 
 user_input = st.text_input("Ask a question:")
 if st.button("Submit") and user_input:
