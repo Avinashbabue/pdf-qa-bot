275cb1a6bf19cdc7989401e9d766043d4ac7968a
Avinashbabue <avinashbabue1926@gmail.com>
2025-07-18 12:23:28 +0530

    Create prompt.py

---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+prompt_template = """
+Use the following context to answer the user's question. 
+If the answer is not present in the context, respond with "I don't know." Do not try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Helpful answer:
+"""
